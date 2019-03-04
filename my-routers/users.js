e035ba4533cfaf35c1ccf616fa9744c92b621e2a
Annie <clara0032@gmail.com>
2019-03-04 22:35:58 +0800

    部落格新刪修完成

--- my-routers/users.js
+++ my-routers/users.js
@@ -15,10 +15,18 @@ router.use((req, res, next) => {
     next();
   }
 });
+//儲存景點
+router.post('/spot:area?',(req,res)=>{
+    const spot = require('./data/spot.json')
+    
+    // console.log(req.body);
+});
+//儲存景點顯示
 router.get("/savespot", (req, res) => {
   const data = res.locals.renderData;
   res.render("savespot", data);
 });
+//個人部落格顯示
 router.get("/user-blog", (req, res) => {
   const data = res.locals.renderData;
   db.query(
@@ -32,9 +40,9 @@ router.get("/user-blog", (req, res) => {
       }
       data.blog = results;
       res.render("userblog", data);
-    }
-  );
+    });
 });
+//發布文章
 router.post("/user-blog", (req, res) => {
   const data = res.locals.renderData;
   const val = {
@@ -44,8 +52,65 @@ router.post("/user-blog", (req, res) => {
     createtime: moment().format("YYYY-MM-DD HH:mm:ss")
   };
   db.query("insert into post set ?", val, (error, results, fields) => {
-    console.log(results);
     res.redirect("/user-blog");
   });
+});
+//修改顯示
+router.get("/user-blog/edit/:title", (req, res) => {
+  const data = res.locals.renderData;
+  db.query(
+    "select * from post where admin_id=? AND title=?",
+    [data.loginUser,req.params.title],
+    (error, results, fields) => {
+      if (!results.length) {
+        res.status(404);
+        res.send("NO Data");
+      } else {
+        data.item = results[0];
+        res.render("user-blog-edit", data);
+      }
+    });
+});
+//修改發布
+router.post("/user-blog/edit/:title", (req, res) => {
+  const data = res.locals.renderData;
+  let my_result = {
+    success: false,
+    affectedRows: 0,
+    info: "內容需輸入",
+  };
+  const val = {
+    title: req.body.title,
+    message: req.body.message,
+    createtime:moment().format("YYYY-MM-DD HH:mm:ss")
+  };
+  if (!req.body.message) {
+    res.json(my_result);
+    return;
+  }
+   db.query("UPDATE `post` SET ? WHERE admin_id=? AND title=?", [val, data.loginUser,req.body.title], (error, results, fields) => {
+      if (error) {
+        console.log(error);
+        res.send(error.sqlMessage);
+        return;
+      }else{
+        my_result = {
+          success: true,
+          affectedRows: 1,
+          info: "修改成功",
+        };
+        res.json(my_result);
+       }
+    });
+});
+//刪除
+router.get("/user-blog/remove/:title", (req, res) => {
+  // console.log(req.params.title);
+  db.query(
+    "delete from post where title=?",
+    [req.params.title],
+    (error, results, fields) => {
+      res.redirect("/user-blog");
+    });
 });
 module.exports = router;
